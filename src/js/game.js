f159db4c9f1f71e09313324ad56e874f5956944e
Nielsen, Clinton <clinton_nielsen@intuit.com>
2022-04-08 12:56:58 -0700

    feat: enable save and load

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,4 @@
-import { print, clear, prompt, wait } from './tools';
+import { print, clear, prompt, wait, saveCharacter, load } from './tools';
 
 import * as CHARACTERS from './definitions/Character';
 import * as LOCATIONS from './definitions/locations';
@@ -9,17 +9,25 @@ import { explore } from './explore';
 async function game() {
   clear();
   const name = await prompt('What is your name?');
-  print('Hi ' + name + ' Welcome to The Game\n');
-  const characterChoice = await prompt(
-    `What character would you like to be?\n<${CHARACTERS.RED}\n<${CHARACTERS.BLUE}\n<${CHARACTERS.GREEN}\n<${CHARACTERS.PURPLE}\n`,
-  );
-  print('You picked ' + characterChoice);
-  await wait(2000);
 
-  let character = createCharacter(name, LOCATIONS.CAVE, characterChoice);
+  let character;
+
+  if (name === 'load') {
+    character = await load();
+  } else {
+    print('Hi ' + name + ' Welcome to The Game\n');
+    const characterChoice = await prompt(
+      `What character would you like to be?\n<${CHARACTERS.RED}\n<${CHARACTERS.BLUE}\n<${CHARACTERS.GREEN}\n<${CHARACTERS.PURPLE}\n`,
+    );
+    print('You picked ' + characterChoice);
+    character = createCharacter(name, LOCATIONS.CAVE, characterChoice);
+  }
+  await wait(2000);
 
   let exit = false;
   while (!exit) {
+    saveCharacter(character);
+
     switch (character.mode) {
       case MODES.EXPLORING:
         character = await explore(character);
--- src/js/tools.js
+++ src/js/tools.js
@@ -1,4 +1,5 @@
 import promptPkg from 'prompt';
+import fs from 'fs';
 
 export function print(string) {
   console.log(string);
@@ -8,13 +9,50 @@ export function clear() {
   console.clear();
 }
 
+let character = null;
+export function saveCharacter(c) {
+  character = c;
+}
+
+export async function save(saveName) {
+  try {
+    await fs.writeFileSync(
+      './savedGames.json',
+      JSON.stringify(
+        {
+          [saveName]: character,
+        },
+        null,
+        2,
+      ),
+    );
+  } catch (_) {
+    console.error('Sorry, there was an issue saving your game :(');
+  }
+}
+
+export async function load() {
+  try {
+    const games = JSON.parse(await fs.readFileSync('./savedGames.json'));
+    print('Here are your saved games.');
+    const gameKeys = Object.keys(games);
+    gameKeys.forEach((gameKey) => print(gameKey + '\n'));
+    const game = await prompt('Which game would you like to load?', gameKeys);
+    return games[game];
+  } catch (_) {
+    console.error('Sorry, there was an issue loading your game :(');
+  }
+}
+
 export async function prompt(description, availableActions) {
   const RESULT = 'result';
   let needsHelp = true;
+  let repeatInput = false;
   let action = '';
 
-  while (needsHelp) {
+  while (needsHelp || repeatInput) {
     needsHelp = false;
+    repeatInput = false;
     promptPkg.message = '';
     promptPkg.start();
     const schema = {
@@ -29,16 +67,16 @@ export async function prompt(description, availableActions) {
 
     if (action === 'exit') {
       clear();
-      print('Goodbye');
+      print('Goodbye 😢\nCome back to ⚔️  another day!');
       process.exit();
     }
 
-    if (action === 'save') {
-      // TODO: do something!
-    }
-
-    if (action === 'load') {
-      // TODO: do something!
+    if (action === 'save' && character != null) {
+      const saveName = await prompt('Please choose a name for your save');
+      await save(saveName);
+      print('Your game has been saved');
+      repeatInput = true;
+      continue;
     }
 
     if (action === 'help' || (availableActions && !availableActions.includes(action))) {
