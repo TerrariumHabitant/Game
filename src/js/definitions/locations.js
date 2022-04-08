8289fa13c3a793c37030476ae384954f8c55bd5b
TerrariumHabitant <talitha.l.nielsen@gmail.com>
2022-04-07 22:54:49 -0600

    fix: typos

--- src/js/definitions/locations.js
+++ src/js/definitions/locations.js
@@ -26,242 +26,242 @@ export const TUNNEL2 = "tunnel2";
 export const TUNNEL3 = "tunnel3";
 export const MINE = "mine";
 export const SECONDTUNNEL1 = "secondtunnel1";
-export const  cavern = "cavern";
+export const CAVERN = "cavern";
 
 export function locations(character) {
 
-    return {
-        [CAVE]: {
-            "description": commonPlaceDescriptions.cave + "You hold your flash light out in front of you and in the dim light you can faintly see a big, old oak door. You walk up What do you want to do?\n",
-            "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
-            [ACTIONS.RUN]: {
-              location: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
-              mode: MODES.EXPLORING,
-            },
-            [ACTIONS.HIDE]: {
-              location: CAVE,
-              mode: MODES.EXPLORING
-            }
-        },
-        [TUNNEL1]: {
-            "description": commonActionDescriptions.tunnel1,
-            "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
-            [ACTIONS.RUN] : {
-              loction: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
-              mode: MODE.EXPLORING 
-            },
-            [ACTIONS.HIDE] : {
-              location: TUNNEL1,
-              mode: MODES.EXPLORING
-            }
-        },
-        [TUNNEL2] : {
-          "description" : commonActionDescriptions.tunnel2, 
-          "forbidden_acts" : [ACTIONS.SWIM, ACTIONS.FLY],
-          [ACTION.RUN] : {
-            loction: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
-            mode: MODES.EXPLORING
-          },
-          [ACTIONS.HIDE] : {
-            loction: TUNNEL2,
-            mode: MODE.EXPLORING
-          }
-        },
-        [TUNNEL3] : {
-          "description": commonActionDescriptions.tunnel3, 
-          "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
-          [ACTION.RUN] : {
-            location: random.randomSelection([]),
-            mode: MODE.EXPLORING
-          },
-          [ACTIONS.HIDE] : {
-            location: TUNNEL3,
-            mode: MODE.EXPLORING
-          }
-        },
-        [MINE]: {
-          "description": commonActionDescriptions.mine,
-          "forbidden_acts": [ACTION.SWIM, ACTION.FLY],
-          [ACTION.RUN] : {
-            loction : random.randomSelection([]),
-            mode: MODE.EXPLORING
-          },
-          [ACTIONS.HIDE] : {
-            location: MINE,
-            mode: MODE.EXPLORING
-          }
-        },
-        [secondtunnel1]: {
-          "description": commonActionDescriptions.secondtunnel1,
-          "forbidden_acts": [ACTION.SWIM, ACTION.FLY],
-          [ACTION.RUN] : {
-            location: random.randomSelection([]),
-            mode: MODE.EXPLORING
-          },
-          [ACTION.HIDE] : { 
-            location: randomSelection([]),
-            mode: MODE.EXPLORING 
-          }
-        },
-        [cavern]: {
-          "description": commonActionDescriptions.cavern,
-          "forbidden_acts": [ACTION.SWIM, ACTION.FLY],
-          [ACTION.RUN] : {
-            location: random.randomSelection([]),
-            mode: MODE.EXPLORING
-          },
-          [ACTION.HIDE] : { 
-            location: randomSelection([]),
-            mode: MODE.EXPLORING 
-          }
-        },
-      }
-  }
-
   return {
-    "cave": {
-      "description": commonPlaceDescriptions.cave + " You hold your flash ligh out in front of you and in the dim light you can faintly see a big, old oak foor. Remember, since you are red you can light on fire in the cave. What do you want to do?\n",
-      "actions": {
-        "run forward": {
-          "result": "tunnel", 
-          "description": commonActionDescriptions.run
+    [CAVE]: {
+        "description": commonPlaceDescriptions.cave + "You hold your flash light out in front of you and in the dim light you can faintly see a big, old oak door. You walk up What do you want to do?\n",
+        "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+        [ACTIONS.RUN]: {
+          location: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
+          mode: MODES.EXPLORING,
         },
-        "run": {
-          "result": "tunnel", 
-          "description": commonActionDescriptions.run
-        },
-        "hide": {
-          "result": "cave",
-          "description": "You duck behind some rocks on your hands and feet and wait a few moments. Nothing happens. What would you like to do now?\n"
-        },
-        "explore": {
-          "result": "cave",
-          "description": "You began towards the door, it's dark and you stumble a lot, the ground is very uneven. As you reach the old door you can see a rusty keyhole, what would you like to do? (type <look for key if you would like to look for a key.\n"
-        },
-        "start on fire": {
-          "result": "cave",
-          "description": "You use your superpower to light yourself on fire, the cave slowly illuminates. You see tall stalactites and stalagmites, in front of you is a big, old, oak door. What would you like to do?\n" //(type <explore to go to the door)
-        },
-        "look for key": {
-          "result": "cave. ",
-          "description": "The cave is very big and you can't see much. You shine your light around and see lots of stalagmites. But a key is not visible."
+        [ACTIONS.HIDE]: {
+          location: CAVE,
+          mode: MODES.EXPLORING
         }
+    },
+    [TUNNEL1]: {
+        "description": commonActionDescriptions.tunnel1,
+        "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+        [ACTIONS.RUN] : {
+          loction: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
+          mode: MODES.EXPLORING
+        },
+        [ACTIONS.HIDE] : {
+          location: randomSelection([TUNNEL1]),
+          mode: MODES.EXPLORING
+        }
+    },
+    [TUNNEL2] : {
+      "description" : commonActionDescriptions.tunnel2, 
+      "forbidden_acts" : [ACTIONS.SWIM, ACTIONS.FLY],
+      [ACTIONS.RUN] : {
+        loction: randomSelection([TUNNEL1, TUNNEL2, TUNNEL3]),
+        mode: MODES.EXPLORING
+      },
+      [ACTIONS.HIDE] : {
+        loction: randomSelection([TUNNEL2]),
+        mode: MODES.EXPLORING
       }
     },
-    "tunnel": {
-      "description": commonPlaceDescriptions.tunnel + " what do you want to do?\n",
-      "actions": {
-        "run forward" : {
-          "result": "mine",
-          "description": "You quickly run forward. "
-        },
-        "run": {
-          "result": Math.floor(Math.random()*2),
-          "description": "You turn quickly and run. "
-        },
-        "hide": {
-          "result": "tunnel",
-          "description": "This tunnel is very narrow, there is no where to hide. Quick, repick!"
-        },
-        "explore": {
-          "result": "mine",
-          "description": "You wander to the end of the tunnel and it begins to open up."
-        },
-        "start on fire": {
-          "result": "tunnel",
-          "description": "You are on fire, you proceed to panic. "
-        },
-        "go back": {
-          "result": "cave",
-          "description": "You are back in the cave now. "
-        }
+    [TUNNEL3] : {
+      "description": commonActionDescriptions.tunnel3, 
+      "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+      [ACTIONS.RUN] : {
+        location: randomSelection([TUNNEL3]),
+        mode: MODES.EXPLORING
+      },
+      [ACTIONS.HIDE] : {
+        location: randomSelection([TUNNEL3]),
+        mode: MODES.EXPLORING
       }
     },
-    "mine": {
-      "description": commonPlaceDescriptions.mine + " what do you want to do?\n",
-      "actions": {
-        "run": {
-          "result": Math.floor(Math.random()*3),
-          "description": " You run quickly, when you've slowed down when you exit the mine. "
-        },
-        "run forward" : {
-          "result": "tunnel",
-          "description": "You quickly run forward. "
-        },
-        "hide" : {
-          "result": "mine",
-          "description": "You dive onto the floor. "
-        },
-        "explore" : {
-          "result": "mine",
-          "description": "You began to walk around the mine. "
-        },
-        "start on fire" : {
-          "result": "mine",
-          "description": "You are in the mine. "
-        },
-        "go back" : {
-          "result": "tunnel",
-          "description": "This is the tunnel. "
-        }
+    [MINE]: {
+      "description": commonActionDescriptions.mine,
+      "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+      [ACTIONS.RUN] : {
+        loction : randomSelection([MINE]),
+        mode: MODES.EXPLORING
+      },
+      [ACTIONS.HIDE] : {
+        location: randomSelection([MINE]),
+        mode: MODES.EXPLORING
       }
     },
-    "second tunnel": {
-      "description": commonPlaceDescriptions.secondtunnel + " What do you want to do?\n", 
-      "actions": {
-        "run" : {
-          "result": Math.floor(Math.random()*4),
-          "description": "You run quickly. "
-        },
-        "run forward" : {
-          "result": "cavern",
-          "description": "You quickly run forward. "
-        },
-        "hide" : {
-          "result": "second tunnel. ",
-          "description": "You jump behind a pile of rocks."
-        },
-        "explore" : {
-          "result": " Second tunnel. ",
-          "desription": "You begin to walk carefully around through the tunnel. It is very narrow and there is not much to see. "
-        },
-        "start on fire" : {
-          "result": "second tunnel. ",
-          "description": "You begin to feel very hot, you look around and you can see that the walls are rough and filled with crevices and bumps. "
-        },
-        "go back" : { 
-          "result": "mine. ",
-          "description": "You go back to the mine. "
-        }
+    [SECONDTUNNEL1]: {
+      "description": commonActionDescriptions.secondtunnel1,
+      "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+      [ACTIONS.RUN] : {
+        location: randomSelection([SECONDTUNNEL1]),
+        mode: MODES.EXPLORING
+      },
+      [ACTIONS.HIDE] : { 
+        location: randomSelection([SECONDTUNNEL1]),
+        mode: MODES.EXPLORING 
       }
     },
-    "cavern": {
-      "description": commonPlaceDescriptions.cavern + " what do you want to do?\n",
-      "actions": {
-        "run forward" : {
-          "result": "cavern",
-          "description": "You quickly run forward, but it is a dead end. "
-        },
-        "run": {
-          "result": Math.floor(Math.random()*5),
-          "description": "You start running. "
-        },
-        "hide" : {
-          "result": "cavern",
-          "description": "This is a cavern. "
-        }, 
-        "explore" : { 
-          "result": "second tunnel",
-          "description": "It is very dark you find a tunnel and walk down it, you realize you have already been down this tunnel. "
-        },
-        "start on fire" : {
-          "result": "cavern",
-          "description": "You are in a cavern. "
-        },
-        "go back" : {
-          "result": "second tunnel",
-          "description": " You are now back in the mine. "
-        }
+    [CAVERN]: {
+      "description": commonActionDescriptions.cavern,
+      "forbidden_acts": [ACTIONS.SWIM, ACTIONS.FLY],
+      [ACTIONS.RUN] : {
+        location: randomSelection([CAVERN]),
+        mode: MODES.EXPLORING
+      },
+      [ACTIONS.HIDE] : { 
+        location: randomSelection([CAVERN]),
+        mode: MODES.EXPLORING 
       }
     }
+  }
 }
+
+//   return {
+//     "cave": {
+//       "description": commonPlaceDescriptions.cave + " You hold your flash ligh out in front of you and in the dim light you can faintly see a big, old oak foor. Remember, since you are red you can light on fire in the cave. What do you want to do?\n",
+//       "actions": {
+//         "run forward": {
+//           "result": "tunnel", 
+//           "description": commonActionDescriptions.run
+//         },
+//         "run": {
+//           "result": "tunnel", 
+//           "description": commonActionDescriptions.run
+//         },
+//         "hide": {
+//           "result": "cave",
+//           "description": "You duck behind some rocks on your hands and feet and wait a few moments. Nothing happens. What would you like to do now?\n"
+//         },
+//         "explore": {
+//           "result": "cave",
+//           "description": "You began towards the door, it's dark and you stumble a lot, the ground is very uneven. As you reach the old door you can see a rusty keyhole, what would you like to do? (type <look for key if you would like to look for a key.\n"
+//         },
+//         "start on fire": {
+//           "result": "cave",
+//           "description": "You use your superpower to light yourself on fire, the cave slowly illuminates. You see tall stalactites and stalagmites, in front of you is a big, old, oak door. What would you like to do?\n" //(type <explore to go to the door)
+//         },
+//         "look for key": {
+//           "result": "cave. ",
+//           "description": "The cave is very big and you can't see much. You shine your light around and see lots of stalagmites. But a key is not visible."
+//         }
+//       }
+//     },
+//     "tunnel": {
+//       "description": commonPlaceDescriptions.tunnel + " what do you want to do?\n",
+//       "actions": {
+//         "run forward" : {
+//           "result": "mine",
+//           "description": "You quickly run forward. "
+//         },
+//         "run": {
+//           "result": Math.floor(Math.random()*2),
+//           "description": "You turn quickly and run. "
+//         },
+//         "hide": {
+//           "result": "tunnel",
+//           "description": "This tunnel is very narrow, there is no where to hide. Quick, repick!"
+//         },
+//         "explore": {
+//           "result": "mine",
+//           "description": "You wander to the end of the tunnel and it begins to open up."
+//         },
+//         "start on fire": {
+//           "result": "tunnel",
+//           "description": "You are on fire, you proceed to panic. "
+//         },
+//         "go back": {
+//           "result": "cave",
+//           "description": "You are back in the cave now. "
+//         }
+//       }
+//     },
+//     "mine": {
+//       "description": commonPlaceDescriptions.mine + " what do you want to do?\n",
+//       "actions": {
+//         "run": {
+//           "result": Math.floor(Math.random()*3),
+//           "description": " You run quickly, when you've slowed down when you exit the mine. "
+//         },
+//         "run forward" : {
+//           "result": "tunnel",
+//           "description": "You quickly run forward. "
+//         },
+//         "hide" : {
+//           "result": "mine",
+//           "description": "You dive onto the floor. "
+//         },
+//         "explore" : {
+//           "result": "mine",
+//           "description": "You began to walk around the mine. "
+//         },
+//         "start on fire" : {
+//           "result": "mine",
+//           "description": "You are in the mine. "
+//         },
+//         "go back" : {
+//           "result": "tunnel",
+//           "description": "This is the tunnel. "
+//         }
+//       }
+//     },
+//     "second tunnel": {
+//       "description": commonPlaceDescriptions.secondtunnel + " What do you want to do?\n", 
+//       "actions": {
+//         "run" : {
+//           "result": Math.floor(Math.random()*4),
+//           "description": "You run quickly. "
+//         },
+//         "run forward" : {
+//           "result": "cavern",
+//           "description": "You quickly run forward. "
+//         },
+//         "hide" : {
+//           "result": "second tunnel. ",
+//           "description": "You jump behind a pile of rocks."
+//         },
+//         "explore" : {
+//           "result": " Second tunnel. ",
+//           "desription": "You begin to walk carefully around through the tunnel. It is very narrow and there is not much to see. "
+//         },
+//         "start on fire" : {
+//           "result": "second tunnel. ",
+//           "description": "You begin to feel very hot, you look around and you can see that the walls are rough and filled with crevices and bumps. "
+//         },
+//         "go back" : { 
+//           "result": "mine. ",
+//           "description": "You go back to the mine. "
+//         }
+//       }
+//     },
+//     "cavern": {
+//       "description": commonPlaceDescriptions.cavern + " what do you want to do?\n",
+//       "actions": {
+//         "run forward" : {
+//           "result": "cavern",
+//           "description": "You quickly run forward, but it is a dead end. "
+//         },
+//         "run": {
+//           "result": Math.floor(Math.random()*5),
+//           "description": "You start running. "
+//         },
+//         "hide" : {
+//           "result": "cavern",
+//           "description": "This is a cavern. "
+//         }, 
+//         "explore" : { 
+//           "result": "second tunnel",
+//           "description": "It is very dark you find a tunnel and walk down it, you realize you have already been down this tunnel. "
+//         },
+//         "start on fire" : {
+//           "result": "cavern",
+//           "description": "You are in a cavern. "
+//         },
+//         "go back" : {
+//           "result": "second tunnel",
+//           "description": " You are now back in the mine. "
+//         }
+//       }
+//     }
+// }
