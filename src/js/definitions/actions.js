1d2a236b529c66fb9f13da737eb7672f9cc7b3d1
TerrariumHabitant <talitha.l.nielsen@gmail.com>
2022-05-12 17:04:51 -0600

    Add actions

--- src/js/definitions/actions.js
+++ src/js/definitions/actions.js
@@ -4,3 +4,15 @@ export const HIDE = `hide${SHORTCUT_DELIMITER}s`;
 export const SWIM = `swim${SHORTCUT_DELIMITER}x`;
 export const FLY = `fly${SHORTCUT_DELIMITER}q`;
 export const FIGHT = `fight${SHORTCUT_DELIMITER}d`;
+
+export const DESCRIPTION = {
+  [RUN]: "run",
+  [FIGHT]: "fight",
+}
+
+export const FORBIDDEN_ACTS = {
+ [SWIM]: "swim",
+ [FLY]: "fly"
+}
+
+export const ALL_ACTIONS = [RUN, HIDE, SWIM, FLY, FIGHT];
